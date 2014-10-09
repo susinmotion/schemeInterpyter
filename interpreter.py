166094ae92d4346d2bd8a10881fa4557f463d6e4
SusInMotion <steinman.tutoring@gmail.com>
2014-10-09 14:32:05 -0400

    refactoring

--- interpreter.py
+++ interpreter.py
@@ -5,24 +5,14 @@ def tokenize(theString):
 	newString=theString.replace("(", " ( ").replace(")"," ) ")
 	tokensList=newString.split()
 	return tokensList
-	"""if tokensList[0] !="(":
-		nestedList=checkfor_single(tokensList)
-	else:
-		throwaway, nestedList= nestListOnPeren(tokensList)
-	return nestedList"""
 
-def output(theString):
+def listify(theString):
 	tokensList=theString
 	if type(theString)==str:
 		tokensList=tokenize(theString)
 	
 	throwaway, returnvalue= nestListOnPeren(tokensList)
 	return returnvalue[0]
-#def getNextItem(index, theList):
-#	if index>len(theList):
-#		return None
-#
-#	return theList[index]
 
 def nestListOnPeren(tokensList):
 
@@ -41,11 +31,26 @@ def nestListOnPeren(tokensList):
 			return index, nestedList
 
 		else:
-			nestedList.append(typeOfItem(item))
+			nestedList.append(item)
 			#index= index+1
 	return index, nestedList
 
-def typeOfItem(token):
+
+def findInnerMost(nestedList):
+	returnList=[]
+	if nestedList[0]=="define":
+		define(nestedList[1:])
+		return None
+	for items in nestedList:
+		if type(items)==list:
+			returnList.append(operate(findInnerMost(items)))
+
+		else:
+			returnList.append(toInt(items))
+	return returnList
+
+
+def toInt(token):
 	typeToken=None
 	try:
 		integer=int(token)
@@ -63,23 +68,6 @@ def typeOfItem(token):
 		else:
 			typeToken="unknown"
 			return (token)
-
-
-def findInnerMost(nestedList):
-	returnList=[]
-	if nestedList[0]=="define":
-		define(nestedList[1:])
-		return None
-	for items in nestedList:
-		if type(items)==list:
-			returnList.append(operate(findInnerMost(items)))
-
-		else:
-			returnList.append(items)
-	return returnList
-
-
-
 """def checkfor_single(scheme_item):
 	if len(scheme_item)>1:
 		print("invalid syntax")
@@ -90,80 +78,39 @@ def findInnerMost(nestedList):
 
 def evaluate(nestedList):
 	#print operate(findInnerMost(tokenize(nestedList)))
-	return operate(findInnerMost(output(nestedList)))
+	return operate(findInnerMost(listify(nestedList)))
 
 def domath(operator, listOfOperands):
-	for items in listOfOperands:
+	"""for items in listOfOperands:
 		if type(items) != int:
 			return None
 	else:
-		result=operators[operator](listOfOperands[0],listOfOperands[1])
-		if len(listOfOperands)>2:
-			restOfList=listOfOperands[2:]
-			restOfList.insert(0,result)
-			result=domath(operator,restOfList)
+		pass"""
+	result=operators[operator](listOfOperands[0],listOfOperands[1])
+	if len(listOfOperands)>2:
+		restOfList=listOfOperands[2:]
+		restOfList.insert(0,result)
+		result=domath(operator,restOfList)
 
-		return result
+	return result
 
 def operate(scheme_item):
 	result=scheme_item
-	if len(scheme_item)==1:
-		return result
-	else:
+	if len(scheme_item)>1:
 		operator=scheme_item[0]
-	index=0
-	for items in scheme_item[1:]:
-		index=index+1
-		if type(items)==list:
+		index=0
+		for items in scheme_item[1:]:
+			index=index+1
 			scheme_item[index]==findInnerMost(scheme_item)
-	if operator in operators.keys():
-		try:
-			result=domath(operator,scheme_item[1:])
-		except IndexError:
-			result=operators[operator](scheme_item[1:])
+		
 
-#this is getting done too many times when we run this function on a homemade function
-
-	"""if operator in creators.keys():
-		creators[operator](scheme_item[1:])
-		return None"""
-
-
+		if operator in operators.keys():
+			try:
+				print "trying to do math"
+				result=domath(operator,scheme_item[1:])
+			except IndexError:
+				result=operators[operator](scheme_item[1:])
 	return result
-
-
-def add(listOfOperands):
-	result=0
-	for items in listOfOperands:
-		result=result+int(items)
-	return result
-
-def subtract(listOfOperands):
-	result=int(listOfOperands[0])
-	for items in listOfOperands[1:]:
-		result=result-int(items)
-	return result
-
-def multiply(listOfOperands):
-	result=1
-	for items in listOfOperands:
-		result=result*int(items)
-	return result
-
-def divide(listOfOperands):
-	numerator=float(listOfOperands[0])
-	for items in listOfOperands[1:]:
-		numerator=numerator/float(items)
-
-	return numerator
-
-def less(listOfOperands):
-	firstNum=listOfOperands[0]
-	for nums in listOfOperands[1:]:
-		if nums > firstNum:
-			return False
-	return True
-
 
 def define(listOfOperands):
 	if len(listOfOperands)!=2:
@@ -189,9 +136,8 @@ def makeFun(expression, params, names):
 	for i in range(len(names)):
 		dictOfVars[names[i]]=params[i]
 
-	for i in range(len(expression)):
-		expression[i] = typeOfItem(expression[i])
-	print expression
+	"""for i in range(len(expression)):
+		expression[i] = typeOfItem(expression[i])"""
 	result= operate (expression)
 	return result
 
